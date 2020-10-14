2957e87524965d45c6d7eaed12dd2bb8b34d2752
Rene <36115705+rene78@users.noreply.github.com>
2020-10-15 00:16:56 +0200

    Demo User hidden

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import './components/app-picturetable.js';
 import './components/app-chart.js';
 
 // Uncommend lines below to load example user data for testing purposes
-import PubSub from './data/pubsub.js';
-import { demoTitleAndCount } from './data/examples.js';
-const pubsub = PubSub;
-pubsub.publish('PicsDownload', demoTitleAndCount, "Demo");
+// import PubSub from './data/pubsub.js';
+// import { demoTitleAndCount } from './data/examples.js';
+// const pubsub = PubSub;
+// pubsub.publish('PicsDownload', demoTitleAndCount, "Demo");
